2d3caf27c0f319c836a51ddd8fa0f15959970ffd
off_jpedro <oficial.jpedroribeiro@gmail.com>
2025-02-23 12:56:13 -0300

    mudança na arquitetura

--- BrGaapFiscal.Api/Models/Fornecedor.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BrGaapFiscal.Api.Models
-{
-    public class Fornecedor
-    {
-        public int Id { get; set; }
-        public string Nome { get; set; }
-    }
-}
--- BrGaapFiscal.Api/obj/Debug/net8.0/BrGaapFiscal.Api.AssemblyInfo.cs
+++ BrGaapFiscal.Api/obj/Debug/net8.0/BrGaapFiscal.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BrGaapFiscal.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+785492caa04b2bd93922fff534b180edfe77b61a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BrGaapFiscal.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BrGaapFiscal.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
